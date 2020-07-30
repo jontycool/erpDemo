80f149cdf8bc7f8d031dea20098771899b0af219
Tathagat Mohanty <iamjonty007@gmail.com>
2020-07-30 15:11:35 +0530

    Added Difference in minutes & Improved Readabilty

--- src/components/UserActivity.js
+++ src/components/UserActivity.js
@@ -7,17 +7,24 @@ function UserActivity({ start, end }) {
   return (
     <div className='eachActivity'>
       <CircularProgressbar
-        value={diff}
-        maxValue={24}
-        text={`${diff}hrs`}
+        value={diff[0] === 0 ? diff[1] : diff[0]}
+        maxValue={diff[0] === 0 ? 1440 : 24}
+        text={diff[0] === 0 ? `${diff[1]}mins` : `${diff[0]}hrs`}
         styles={{
           background: {
             fill: '#008080',
           },
         }}
       />
-      <h5>Start Time: {start}</h5>
-      <h5>End Time: {end}</h5>
+      <div className='time-container'>
+        <div className='firstLine'>
+          <h5>Start Time: {start}</h5>
+          <h5>End Time: {end}</h5>
+        </div>
+        <div className='secondLine'>
+          <h5>Total Time: {(diff[1] / 60).toFixed(2)} hrs</h5>
+        </div>
+      </div>
     </div>
   );
 }
@@ -25,9 +32,12 @@ function UserActivity({ start, end }) {
 function calculateHours(dt2, dt1) {
   dt1 = new Date(dt1);
   dt2 = new Date(dt2);
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= 60 * 60;
-  return Math.abs(Math.round(diff));
+  var diffhr = (dt2.getTime() - dt1.getTime()) / 1000;
+  var diffmin = (dt2.getTime() - dt1.getTime()) / 1000;
+  diffhr /= 60 * 60;
+  diffmin /= 60;
+  var timeDiff = [Math.abs(Math.round(diffhr)), Math.abs(Math.round(diffmin))];
+  return timeDiff;
 }
 
 export default UserActivity;
